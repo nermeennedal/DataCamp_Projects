b163389d6c64314427c78bbef05a3d88f7739285
Nermeen Nedal <nermeennedal11@gmail.com>
2026-03-28 21:34:29 +0300

    code

--- KNN.py
+++ KNN.py
@@ -3,10 +3,12 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
 df = pd.read_csv("data.csv")
-Y=df["y"].values
-X=df.drop(columns=["y"]).values
-print(X.shape)
-
+Y_train=df["y"].values
+X_train=df.drop(columns=["y"]).values
+X_test=pd.read_csv("test.csv").values
+# print(X_test)
 ############# MODEL INSTANTEIATION ######################
 KNN=KNeighborsClassifier(2)
-KNN.fit(X,Y)
+KNN.fit(X_train,Y_train)
+prediction=KNN.predict(X_test)
+print(prediction)
