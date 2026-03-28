668ab468b42fbd4e7a18e177b1ad39fa57332e5d
Nermeen Nedal <nermeennedal11@gmail.com>
2026-03-29 00:18:52 +0300

    code

--- KNN.py
+++ KNN.py
@@ -1,14 +1,22 @@
 #code
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split 
 import pandas as pd
 import numpy as np
 df = pd.read_csv("data.csv")
-Y_train=df["y"].values
-X_train=df.drop(columns=["y"]).values
-X_test=pd.read_csv("test.csv").values
+Y=df["y"].values
+X=df.drop(columns=["y"]).values
+x_train,x_test,y_train,y_test=train_test_split(X,Y)
+
+x=pd.read_csv("test.csv").values
 # print(X_test)
 ############# MODEL INSTANTEIATION ######################
-KNN=KNeighborsClassifier(2)
-KNN.fit(X_train,Y_train)
-prediction=pd.DataFrame(KNN.predict(X_test))
-print(prediction)
+KNN=KNeighborsClassifier(3)
+KNN.fit(x_train,y_train)
+accurecy=KNN.score(x_test,y_test)
+prediction=pd.DataFrame(KNN.predict(x))
+print(accurecy)
+
+
+
+ 
