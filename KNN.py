b429f8a630966b78ce82453d1c290830596e4f16
Nermeen Nedal <nermeennedal11@gmail.com>
2026-04-24 20:17:25 +0300

    nemo

--- KNN.py
+++ KNN.py
@@ -1,22 +1,15 @@
-#code
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split 
 import pandas as pd
 import numpy as np
-df = pd.read_csv("data.csv")
-Y=df["y"].values
-X=df.drop(columns=["y"]).values
-x_train,x_test,y_train,y_test=train_test_split(X,Y)
 
-x=pd.read_csv("test.csv").values
-# print(X_test)
-############# MODEL INSTANTEIATION ######################
-KNN=KNeighborsClassifier(3)
-KNN.fit(x_train,y_train)
-accurecy=KNN.score(x_test,y_test)
-prediction=pd.DataFrame(KNN.predict(x))
-print(accurecy)
+Dataset=pd.DataFrame(pd.read_csv('student_performance.csv'))
+Dataset.dropna(inplace=True)
+
+# split the dataset into dependent and independent variables 
+student_id=Dataset['student_id']
+print(student_id)
+X=Dataset.iloc[:,1:9]
+Y=Dataset.iloc[:,9:11]
+print(X)
 
 
 
- 
