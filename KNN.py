f19be8187cb9f2d533a6dae4ccefaf5c68b805ee
Nermeen Nedal <nermeennedal11@gmail.com>
2026-04-24 20:30:14 +0300

    nemo

--- KNN.py
+++ KNN.py
@@ -3,13 +3,19 @@ import numpy as np
 
 Dataset=pd.DataFrame(pd.read_csv('student_performance.csv'))
 Dataset.dropna(inplace=True)
-
+Dataset['gender']=Dataset['gender'].replace({'Male':0,'Female':1})
+Dataset['parent_education']=Dataset['parent_education'].replace({'High School':0,'Bachelor':1,'Master':2,'PhD':3})
+Dataset['passed']=Dataset['passed'].replace({'No':0,'Yes':1})
 # split the dataset into dependent and independent variables 
 student_id=Dataset['student_id']
 print(student_id)
 X=Dataset.iloc[:,1:9]
-Y=Dataset.iloc[:,9:11]
-print(X)
+Y=Dataset.iloc[:,10:11]
+
+print(Y)
+
+
+
 
 
 
